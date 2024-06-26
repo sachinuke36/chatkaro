91fc95d53b2da596e798c9d018bf1793876e6fb7
sachin krupal uke <sachinkrupaluke@sachins-MacBook-Air.local>
2024-06-26 16:47:15 +0530

    frontend url added

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port =  process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://chatkaro5.vercel.app',
     credentials:true
 }))
 
